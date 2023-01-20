00442807fcb565da034097203ee0c791f5855a18
WeiEn Chua <weien_chua@aiap.sg>
2023-01-20 20:04:01 +0800

    fix: fixed recursion error by using stack

--- src/run.py
+++ src/run.py
@@ -1,48 +1,49 @@
 from typing import List
 import sys
 
-from validate import validate_inputs
+from src.validate import validate_inputs
 
 
 class IslandsCounting:
 
-    def dfs(self, grid: List, x: int, y: int) -> None:
+    def dfs(self, grid: List[int], x: int, y: int) -> None:
         """Using DFS to find all the "1" in the same island
 
         :param List grid: input grid
         :param int x: x coordinate
         :param int y: y coordinate
         """
-        if x < 0 or y < 0 or x >= len(grid) or y >= len(grid[x]) or grid[x][y] != "1":
-            return
+        stack = [(x, y)]
 
-        # replace the element "1" to "/" to mark that it is already counted
-        if grid[x][y] == "1":
+        while stack:
+            x, y = stack.pop()
             grid[x] = grid[x][:y] + "/" + grid[x][y+1:]
+            search_x = [x+1, x-1, x]
+            search_y = [y+1, y-1, y]
 
-        search_x = [x+1, x-1, x]
-        search_y = [y+1, y-1, y]
+            # list all the 8 possible directions:
+            # (x-1, y+1)        (x, y+1)      (x+1, y+1)
+            # (x-1, y)          (x, y)        (x+1, y)
+            # (x-1, y-1)        (x, y-1)      (x+1, y-1)
 
-        # list all the 8 possible directions:
-        # (x-1, y+1)        (x, y+1)      (x+1, y+1)
-        # (x-1, y)          (x, y)        (x+1, y)
-        # (x-1, y-1)        (x, y-1)      (x+1, y-1)
+            dirs = [(i, j) for i in search_x for j in search_y]
 
-        dirs = [(i, j) for i in search_x for j in search_y]
+            for i, j in dirs:
+                if 0 <= i < len(grid) and 0 <= j < len(grid[0]) and grid[i][j] == "1":
+                    stack.append((i, j))
 
-        for dir in dirs:
-            self.dfs(grid, dir[0], dir[1])
+    def read_input_file(self, input_filepath: str) -> List[str]:
+        with open(input_filepath, "r") as f:
+            grid = [line.strip() for line in f.readlines()]
 
-    def count_islands(self, input_filepath: str) -> int:
+        return grid
+
+    def count_islands(self, grid: List[str]) -> int:
         """Count number of islands in input file using Depth First Search (DFS).
 
         :param str input_filepath: input filepath to .txt file
         :return int: number of islands
         """
-
-        with open(input_filepath, "r") as f:
-            grid = [line.strip() for line in f.readlines()]
-
         # if input is empty file
         if len(grid) == 0:
             raise ValueError("Input is empty!")
@@ -51,6 +52,8 @@ class IslandsCounting:
         validate_inputs(grid)
 
         islands_count = 0
+        visited = [[False for _ in range(len(grid[0]))]
+                   for _ in range(len(grid))]
 
         for x, row in enumerate(grid):
             for y, col in enumerate(row):
@@ -62,5 +65,6 @@ class IslandsCounting:
 
 
 if __name__ == "__main__":
-    counts = IslandsCounting()
-    print(counts.count_islands(sys.argv[1]))
+    islands = IslandsCounting()
+    grid = islands.read_input_file(sys.argv[1])
+    print(islands.count_islands(grid))
